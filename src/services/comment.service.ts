7df6ee544b2893a9819ad325540dd1d5fa2fe554
JiNookk <ojw0828@naver.com>
2025-05-05 20:53:19 +0900

    add: update comment

---
+++ src/services/comment.service.ts
@@ -0,0 +1,71 @@
+import { ApiResponse, IPagination } from "@/types";
+import { axiosInstance } from "./axios.instance";
+import { IComment } from "@/types/comment";
+import { AxiosResponse } from "axios";
+
+export class CommentService {
+  async getComments(postId: number) {
+    try {
+      const response = await axiosInstance.get<
+        ApiResponse<IPagination.IOffset<IComment[]>>
+      >(`/api/v1/posts/${postId}/comments`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async createComment(postId: number, body: string) {
+    try {
+      const response = await axiosInstance.post<
+        ApiResponse<IComment>,
+        AxiosResponse<ApiResponse<IComment>>,
+        IComment.CreateDto
+      >(`/api/v1/posts/${postId}/comments`, { body });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updateComment(commentId: number, body: string) {
+    try {
+      const response = await axiosInstance.patch<
+        ApiResponse<IComment>,
+        AxiosResponse<ApiResponse<IComment>>,
+        IComment.UpdateDto
+      >(`/api/v1/comments/${commentId}`, { body });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteComment(commentId: number) {
+    try {
+      await axiosInstance.delete<ApiResponse<void>>(
+        `/api/v1/comments/${commentId}`
+      );
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async likeComment(commentId: number) {
+    try {
+      const response = await axiosInstance.post<ApiResponse<IComment.ILike>>(
+        `/api/v1/comments/${commentId}/like`
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
+
+export const commentService = new CommentService();
--- types/comment.ts
+++ types/comment.ts
@@ -1,7 +1,25 @@
 export interface IComment {
   id: number;
-  memberId: string;
+  memberId: number;
   body: string;
   likeCount: number;
   createdAt: string;
+}
+
+export namespace IComment {
+  export interface ILike {
+    id: number;
+    memberId: number;
+    body: string;
+    likeCount: number;
+    createdAt: string;
+  }
+
+  export class CreateDto {
+    body!: string;
+  }
+
+  export class UpdateDto {
+    body!: string;
+  }
 }
