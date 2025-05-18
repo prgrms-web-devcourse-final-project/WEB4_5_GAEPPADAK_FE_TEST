1ff45aff9525d6e496ae92a4fe9c64ef1ae63f91
JiNookk <ojw0828@naver.com>
2025-05-18 15:05:15 +0900

    add: email verify service and types

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,12 @@
-import { IAuth } from "../../types";
+import { ApiResponse, IAuth } from "../../types";
 import { axiosInstance } from "./axios.instance";
+import { AxiosResponse } from "axios";
 
 class AuthService {
   async signup(signupDto: IAuth.SignUpDto): Promise<IAuth.SignUpResponse> {
     const response = await axiosInstance.post<IAuth.SignUpResponse>(
       "api/v1/auth/signup",
-      signupDto
+      signupDto,
     );
     try {
       return response.data;
@@ -18,7 +19,7 @@ class AuthService {
   async signin(signinDto: IAuth.SignInDto): Promise<IAuth.SignInResponse> {
     const response = await axiosInstance.post<IAuth.SignInResponse>(
       "api/v1/auth/login",
-      signinDto
+      signinDto,
     );
     try {
       return response.data;
@@ -43,6 +44,28 @@ class AuthService {
     } catch (error) {
       console.error(error);
       throw error;
+    }
+  }
+
+  async sendEmailAuth(): Promise<void> {
+    try {
+      await axiosInstance.post<ApiResponse<void>>("api/v1/verify-email");
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  async checkEmail(body: IAuth.CheckEmailDto): Promise<void> {
+    try {
+      await axiosInstance.post<
+        ApiResponse<void>,
+        AxiosResponse<ApiResponse<void>>,
+        IAuth.CheckEmailDto
+      >("api/v1/check-email", body);
+    } catch (e) {
+      console.error(e);
+      throw e;
     }
   }
 }
